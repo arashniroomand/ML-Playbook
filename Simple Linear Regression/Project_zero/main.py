fc8bb20d1f13a510c7cbfafa79a7ef9476a8bc9f
arashniroumand <arashniroo80@gmail.com>
2025-02-08 19:42:42 +0330

    menu and some functions

--- Simple Linear Regression/Project_zero/main.py
+++ Simple Linear Regression/Project_zero/main.py
@@ -3,18 +3,18 @@ from constant import MENU, OPTIONS
 from utility import set_values
 
 terminal_width = shutil.get_terminal_size().columns  # Get terminal width dynamically
-
 def menu():
-    while True:
+    show_menu = True
+    while show_menu:
         print(MENU)
         print()
         print(OPTIONS)
         option = input("ENTER YOUR CHOICE: ".center(terminal_width))
         match option:
             case '1':
-                set_values('loss')
+                show_menu = set_values('loss')
             case '2':
-                set_values('Gradient')
+                show_menu = set_values('Gradient')
             case '3':  # Exit option
                 print("Exiting...".center(terminal_width))
                 break  # Exit the loop
--- Simple Linear Regression/Project_zero/utility.py
+++ Simple Linear Regression/Project_zero/utility.py
@@ -1,4 +1,5 @@
 import shutil
+import time
 
 import pandas as  pd
 import matplotlib.pyplot as plt 
@@ -29,7 +30,7 @@ def check_values(start_m, end_m, start_b, end_b):
 def gradient_descent(m_now, b_now, points, L):
     m_gradient = 0
     b_gradient = 0
-    n = len(points)
+    n = float(len(points))
     for i in range(len(points)):
         x = points.iloc[i].studytime
         y = points.iloc[i].score
@@ -48,7 +49,17 @@ def find_best_values(m, b, L, epochs):
     print(f"Optimized m: {m}, Optimized b: {b}")
     print(f"Final Loss: {loss_function(m, b, data)}")
 
-
+def show_menu():
+    result = input("DO YOU WANT TO QUIT OR GO TO MENU:(Q: QUIT , M:MENU):").lower()
+    if result == 'q':
+        return False
+    elif result == 'm':
+        return True
+    else:
+        print("ENTER Q OR M. YOU ENTERED INVALID VALUE!!")
+        show_menu()
+        
+        
 def set_values(temp):
     terminal_width = shutil.get_terminal_size().columns  # Get terminal width dynamically
 
@@ -60,11 +71,13 @@ def set_values(temp):
             try:
                 start_m, end_m = map(int, input("ENTER THE START AND THE END OF SPAN FOR m: ".center(terminal_width)).split())
                 start_b, end_b = map(int, input("ENTER THE START AND THE END OF SPAN FOR b: ".center(terminal_width)).split())
-
                 best_m, best_b, final_result = check_values(start_m, end_m, start_b, end_b)
                 print()
-                print(f"{'Optimized m:':<20}{best_m}, {'Optimized b:':<20}{best_b}. {'The loss is':<20}{final_result}".center(terminal_width))
-                return 
+                print(f"{'Optimized m:':<5}{best_m}, {'Optimized b:':<5}{best_b}. {'The loss is':<5}{final_result}".center(terminal_width))
+                result = show_menu()
+                time.sleep(2) 
+                print(result)
+                return result
             except ValueError:
                 print("Invalid input! Please enter two integer values separated by a space.".center(terminal_width))
 
@@ -74,8 +87,13 @@ def set_values(temp):
             b = int(input("PLEASE ENTER THE b VALUE: ".center(terminal_width)))
             loss = loss_function(m, b, data)
             print(f"Loss with {m}*x + {b} is {loss}".center(terminal_width))
+            result = show_menu()
+            time.sleep(2) 
+            return result
         
         elif option == '3':
+            print("Exiting...".center(terminal_width))
+            time.sleep(2)
             return
 
     elif temp == 'Gradient':
